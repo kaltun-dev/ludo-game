7ff51b44a545062d865a2b839ee55a17ad137244
kaltun-dev <kaltun19932014@gmail.com>
2022-10-23 17:41:00 +0000

    added roll dice function

--- pig-game.js
+++ pig-game.js
@@ -1,12 +1,33 @@
 'use strict';
 
-//slecting elements
+//selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySElector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //starting constions
-score0El = textContent = 0;
-score1El = textContent = 0;
-diceEl = classList.add('hidden');
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+
+// rolling the dice functionality
+
+btnRoll.addEventListener('click', function() {
+    //generate a random dice roll
+   const dice = Math.trunck(Math.random()*6) +1;
+
+    // dislay the dice image
+   diceEl.classList.remove('hidden');
+   diceEl.src = `dice-${dice}.png`;
+
+    //check for roll 1: if true switch to next player.
+
+
+})
+
+
 
