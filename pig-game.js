9e053182be8836d1d91a6b1dfa1474921f050dbb
kaltun-dev <kaltun19932014@gmail.com>
2022-10-23 22:43:11 +0000

    addid up the scores for active playe

--- pig-game.js
+++ pig-game.js
@@ -2,7 +2,7 @@
 
 //selecting elements
 const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
@@ -23,6 +23,14 @@ const scores = [0,0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
+
 // rolling the dice functionality
 
 btnRoll.addEventListener('click', function() {
@@ -36,19 +44,28 @@ btnRoll.addEventListener('click', function() {
     //check for roll 1
 if (dice !== 1) {
     currentScore = currentScore + dice; //currentScore += dice;
-    // current0El.textContent = currentScore;
     document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 } else {
  //switch to next player.
- document.getElementById(`current--${activePlayer}`).textContent = 0;
-currentScore = 0;
-activePlayer = activePlayer === 0 ? 1 : 0;
-player0El.classList.toggle('player--active');
-player1El.classList.toggle('player--active');
+ switchPlayer();
 
 
 }
 
+});
+
+btnHold.addEventListener('click', function () {
+// add current scote to active plays score
+scores[activeplayer] += currentScore;
+// same as scores[1] = scores[1] + currentscore
+
+document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+// check if players score is >= 100
+// win finish the game
+
+//and switch to next player
+switchPlayer();
 })
 
 
