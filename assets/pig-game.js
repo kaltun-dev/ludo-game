7e35bbbe6d373dde027ce47125a7141e402beae1
kaltun-dev <kaltun19932014@gmail.com>
2022-10-23 14:38:10 +0000

    linking files

--- assets/pig-game.js
+++ assets/pig-game.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //slecting elements
-const score0El = querySelector('#score--0');
+const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 
