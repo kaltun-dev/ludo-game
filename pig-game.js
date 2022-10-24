7763dce0702750fe4ba0684babfb40b6aa08039c
kaltun-dev <kaltun19932014@gmail.com>
2022-10-24 00:14:15 +0000

    refactoring code

--- pig-game.js
+++ pig-game.js
@@ -14,15 +14,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
+
 //starting constions
+const init = function () {
+scores = [0,0];
+currentScore = 0;
+activePlayer = 0;
+playing = true;
+
 score0El.textContent = 0;
 score1El.textContent = 0;
-diceEl.classList.add('hidden');
+current0El.textContent = 0;
+current1El.textContent =0;
 
-let scores = [0,0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+diceEl.classList.add('hidden');
+player0El.classList.remove('player--winner');
+player1El.classList.remove('player--winner');
+player0El.classList.add('player--active');
+player1El.classList.remove('player--active');
+
+};
+init();
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -30,7 +43,7 @@ const switchPlayer = function () {
     activePlayer = activePlayer === 0 ? 1 : 0;
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
-}
+};
 
 // rolling the dice functionality
 
@@ -50,8 +63,6 @@ if (dice !== 1) {
 } else {
  //switch to next player.
  switchPlayer();
-
-
 }
 }
 });
@@ -79,26 +90,5 @@ document.querySelector(`.player--${activePlayer}`).classList.remove('player--act
 });
 
 
-btnNew.addEventListener('click', function () {
-    //scores = [0,0];
-     currentScore = 0;
-     score0El = 0;
-     score1El = 0;
-     score0El.textContent = 0;
-     score1El.textContent = 0;
-     activePlayer = activePlayer;
-     scores = [0,0];
-     playing = true;
-     diceEl.classList.remove('hidden');
-     document.querySelector('.player--0').classList.remove('player--winner');
-     document.querySelector('.player--1').classList.remove('player--winner');
-     document.querySelector('.player--0').classList.add('player--active');
-     document.querySelector('.player--1').classList.remove('player--active');
-
-
-})
-// reset scores to 0
-// reset hold to zero
-//hide dice
-//show buttuns again
+btnNew.addEventListener('click', init);
 
